22eba2c4723ec3fadb9be56e988ab9c34dff050c
lnwbeer <beer0817@hotmail.com>
2025-01-25 23:04:28 +0700

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'; // Use createRoot from react-dom/client
+import './index.css';
+import Posts, { loader as postsLoader} from './routes/Posts.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './routes/RootLayout.jsx';
+import NewPost, { action as newPostAction} from './routes/NewPost.jsx';
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { 
+        path: '/', 
+        element: <Posts />, 
+        loader: postsLoader,
+        children:[
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:postId', element: <PostDetails/>, loader: postDetailsLoader}
+        ],  
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render( // Fixed typo here
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
